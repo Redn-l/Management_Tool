2ca2a8e48a8699ee50310ea70ab114d925987f8a
Redn <vladim.chd@gmail.com>
2025-05-04 18:28:26 +0300

    last prod vers

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500).json({message: 'Server error!!'});
+  res.status(err.status || 500).json({message: 'Ошибка!'});
 });
 
 module.exports = app;
